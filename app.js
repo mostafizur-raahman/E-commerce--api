deb900c0e16140163c8fd609baf07cc92b71117a
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-10-02 23:29:32 +0600

    add routes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
+const router = require("./src/modules/routes");
+const globalErrorHandler = require("./src/modules/middleware/globalError");
+const Fault = require("./src/modules/middleware/Fault");
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(morgan("dev"));
 
 const server = http.createServer(app);
 
+app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+//routes
 module.exports = server;
