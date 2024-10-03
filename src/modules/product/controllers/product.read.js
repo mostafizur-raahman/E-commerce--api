c35f48757883a962dc26cf554c7c84efe701767c
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-10-03 22:52:59 +0600

    read product with paigination

---
+++ src/modules/product/controllers/product.read.js
@@ -0,0 +1,29 @@
+const Fault = require("../../middleware/Fault");
+const { productModel } = require("../product.model");
+
+const read = async (req, res, next) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+
+        const skip = (page - 1) * limit;
+
+        const totalProducts = await productModel.countDocuments();
+
+        const products = await productModel.find({}).skip(skip).limit(limit);
+
+        return res.status(200).json({
+            message: "Products retrieved successfully",
+            data: products,
+            pagination: {
+                total: totalProducts,
+                page,
+                pages: Math.ceil(totalProducts / limit),
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = read;
